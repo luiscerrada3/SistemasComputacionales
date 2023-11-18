4bd2b00ce895113d21f01be82880adc825a6a3a9
Luis Cerrada <darkriders07@gmail.com>
2023-11-18 10:58:07 -0300

    union de los elementos

--- environment.py
+++ environment.py
@@ -1,99 +1,57 @@
+from unittest import result
 import gym
 import numpy as np
 
+
+
+def GetRewardBKT(action,next_emotion):
+    return '0.66'#np.random.randint(2)
+
+def PositiveReinforcement(action,next_emotion):
+    print("evaluando positivamente")
+
+def NegativeReinforcement(action,next_emotion):
+    print("evaluando negativamente")
+
 class MatematicasEnv(gym.Env):
-    def __init__(self):
+    def __init__(self,skills):
         super(MatematicasEnv, self).__init__()
-        self.observation_space = gym.spaces.Discrete(4)  
+        self.observation_space = gym.spaces.Discrete(6)  
         self.action_space = gym.spaces.Discrete(5)          
         self.current_emotion = None
         self.current_step = 0
-        self.max_happiness_category = None  
+        self.skills = skills
         self.reset()
+        
+    def LoadBKTModel(self,modelBKT):
+        self.modelBKT = modelBKT
+        
+    def LoadHMModel(self,modelHMM):
+        self.modelHMM = modelHMM
 
     def reset(self):
         self.current_emotion = np.random.choice(self.observation_space.n)  
         self.current_step = 0
-        self.max_happiness_category = None 
         return self.current_emotion
 
     def step(self, action):
-        categories = {0: 'suma', 1: 'resta', 2: 'multiplicacion', 3: 'division', 4: 'porcentaje'}
-        study_category = categories[action]
-
-        is_correct_answer = np.random.choice([True, False])
-        reward = self.calculate_reward()
-        self.current_emotion = np.random.choice(self.observation_space.n)
-        self.current_step += 1
-        self.max_happiness_category = self.determine_max_happiness_category(categories)
-
-        #Condicionales con las emociones y la accion(categoria) 
-        base = 0.5  
-        emotion_adjustment = 0
-        category_adjustment = 0
         
-        if self.current_emotion == 0:  
-            emotion_adjustment = 0.2
-        elif self.current_emotion == 1:  
-            emotion_adjustment = 0.1
-        elif self.current_emotion == 2:  
-            emotion_adjustment = -0.1
-        elif self.current_emotion == 3:  
-            emotion_adjustment = -0.2
-
+        #la accion pasa al hmm junto con el estado actual para asi generar el proximo estado
+        next_emotion = self.modelHMM.GeneratEmotionHMM(self.current_emotion)
         
-        if study_category == "suma":  
-            category_adjustment = 0.1
-        elif study_category == "resta":  
-            category_adjustment = 0.1
-        elif study_category == "multiplicacion":  
-            category_adjustment = 0.1
-        elif study_category == "division":  
-            category_adjustment = 0.1
-        elif study_category == "porcentaje":  
-            category_adjustment = 0.1
-
-        if is_correct_answer:  
-            answer_adjustment = emotion_adjustment + category_adjustment
-        else:  
-            answer_adjustment = - (emotion_adjustment + category_adjustment)
-
-        result = base + answer_adjustment
-
-        #probabilidad de correcto
-
+        #Despues de generar la emocion la envio al BKT para generar la prediccion y obtener el reward
+        reward = self.modelBKT.GetRewardBKT(self.skills[action],next_emotion)
+        if float(reward)>= 0.95 :
+            PositiveReinforcement(self.skills[action],next_emotion)
+            result = 1
+        else:
+            NegativeReinforcement(self.skills[action],next_emotion)
+            result = 0
+            
+        
 
         #siguiente emocion
 
-        print("Emoción:", self.get_emotion_name(), "Recompensa:", result, "Categoría de mayor felicidad:", self.get_category_name())
-        return self.current_emotion, reward, False
+        print("Emoción:", self.current_emotion, "Recompensa:", result)
+        return self.current_emotion, result, False
 
-    def calculate_reward(self):
-        #si responde correcto es 1
-        #caso contrario 0
-        base_reward = 0.5  
-        
-        if self.current_emotion == 0:  
-            base_reward = 1
-        else:
-            base_reward = 0
-        
-        return base_reward
-            
-    def determine_max_happiness_category(self, categories):
-        rewards = {}
-
-        for category in categories:
-            reward = self.calculate_reward()
-            rewards[category] = reward
-
-        max_category = max(rewards, key=rewards.get)
-        return max_category
-
-    def get_emotion_name(self):
-        emotion_names = ['feliz', 'triste', 'neutro', 'confundido']
-        return emotion_names[self.current_emotion]
-
-    def get_category_name(self):
-        category_names = ['suma', 'resta', 'multiplicacion', 'division', 'porcentaje']
-        return category_names[self.max_happiness_category]
